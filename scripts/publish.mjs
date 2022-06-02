561c14750adcf3d9d28c9c511158d79ae8efcac9
thezzisu <thezzisu@gmail.com>
2022-06-02 19:59:27 +0800

    chore: bump versions

---
+++ scripts/publish.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { fs, chalk, cd, $ } from 'zx'
+import { targetPackages } from './common.mjs'
+
+cd(join(dirname(fileURLToPath(import.meta.url)), '..'))
+
+const packages = await targetPackages()
+
+const success = [],
+  fail = [],
+  ignored = []
+
+for (const path of packages) {
+  cd(path)
+  if (await fs.pathExists('package.json')) {
+    const json = fs.readJSONSync('package.json')
+    const name = json.name
+    if (!json.scripts?.test) {
+      ignored.push(name)
+      continue
+    }
+    try {
+      console.log(`Publishing package ${chalk.green(name)}`)
+      await $`npm pub`
+      success.push(name)
+    } catch {
+      fail.push(name)
+    }
+  }
+}
+
+if (ignored.length) {
+  console.log(`${chalk.gray('Ignored')}: ${ignored.join(', ')}`)
+}
+if (success.length) {
+  console.log(`${chalk.green('Success')}: ${success.join(', ')}`)
+}
+if (fail.length) {
+  console.log(`${chalk.red('Fail')}: ${fail.join(', ')}`)
+}
--- scripts/versions.mjs
+++ scripts/versions.mjs
@@ -1,26 +1,85 @@
 // @ts-check
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { fs, chalk, cd } from 'zx'
-import { targetPackages } from './common.mjs'
+import { fs, cd, chalk, argv } from 'zx'
+import glob from 'glob-promise'
 
 cd(join(dirname(fileURLToPath(import.meta.url)), '..'))
 
-const packages = await targetPackages()
+async function getPackages() {
+  const packages = await glob('packages/*')
+  return packages
+    .filter((p) => fs.existsSync(join(p, 'package.json')))
+    .map((p) => [p, fs.readJsonSync(join(p, 'package.json'))])
+}
 
-for (const path of packages) {
-  cd(path)
-  if (await fs.pathExists('package.json')) {
-    const json = fs.readJSONSync('package.json')
-    const name = json.name
-    console.group(`${name}: ${chalk.green(json.version)}`)
-    for (const key of Object.keys(json.dependencies || {})) {
-      if (key.startsWith('@chijs')) {
-        console.log(
-          `${chalk.gray(key)}: ${chalk.green(json.dependencies[key])}`
-        )
+const packages = await getPackages()
+const nodes = Object.fromEntries(
+  packages.map((p) => {
+    const [path, { name, version, dependencies, devDependencies }] = p
+    const deps = [
+      ...Object.keys(dependencies ?? {}),
+      ...Object.keys(devDependencies ?? {})
+    ]
+      .filter((x) => x.startsWith('@chijs'))
+      .map((x) => ({ name: x, version: dependencies[x] ?? devDependencies[x] }))
+    return [name, { name, version, path, deps }]
+  })
+)
+
+if (argv.list) {
+  for (const name of Object.keys(nodes)) {
+    console.group(`${chalk.blue(name)}@${chalk.green(nodes[name].version)}`)
+    if (nodes[name].deps.length) {
+      for (const dep of nodes[name].deps) {
+        console.log(`${chalk.yellow(dep.name)}@${chalk.green(dep.version)}`)
       }
+    } else {
+      console.log('No dependencies')
     }
     console.groupEnd()
   }
+}
+
+const problems = []
+
+for (const name of Object.keys(nodes)) {
+  for (const dep of nodes[name].deps) {
+    if (!nodes[dep.name]) {
+      problems.push(
+        `${name} depends on ${dep.name} but it is not in the list of packages`
+      )
+    }
+    if (nodes[dep.name].version !== dep.version.substring(1)) {
+      problems.push(
+        `${chalk.blue(name)} > ${chalk.yellow(dep.name)}: ` +
+          `${chalk.red(dep.version)} -> ${chalk.green(nodes[dep.name].version)}`
+      )
+    }
+  }
+}
+
+if (problems.length) {
+  for (const problem of problems) console.log(problem)
+  if (argv.fix) {
+    for (const name of Object.keys(nodes)) {
+      for (const dep of nodes[name].deps) {
+        if (nodes[dep.name].version !== dep.version.substring(1)) {
+          console.log(`Fixing ${chalk.blue(name)} > ${chalk.yellow(dep.name)}`)
+          const path = join(nodes[name].path, 'package.json')
+          fs.writeFileSync(
+            path,
+            fs
+              .readFileSync(path, 'utf-8')
+              .replace(
+                `"${dep.name}": "${dep.version}"`,
+                `"${dep.name}": "^${nodes[dep.name].version}"`
+              )
+          )
+        }
+      }
+    }
+  }
+} else {
+  console.log(chalk.green('No problems found.'))
 }
