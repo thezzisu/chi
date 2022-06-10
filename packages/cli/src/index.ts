c20cd1b66575cd55dff4852d629a90e30ab434e6
thezzisu <thezzisu@gmail.com>
2022-06-10 17:08:34 +0800

    feat(cli): prefer local cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,31 +1,43 @@
 #!/usr/bin/env node
+import chalk from 'chalk'
+import { fork } from 'node:child_process'
+import { readFileSync } from 'node:fs'
+import { createRequire } from 'node:module'
+import { dirname, join, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import { readFileSync } from 'fs'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
 import { startServer } from './server.js'
 
-const dir = dirname(fileURLToPath(import.meta.url))
-const { version } = JSON.parse(
-  readFileSync(join(dir, '..', './package.json'), 'utf8')
-)
-
-yargs(hideBin(process.argv))
-  .version(version)
-  .scriptName('chi')
-  .command(
-    'serve <config>',
-    'Start Chi Server',
-    (yargs) =>
-      yargs.positional('config', {
-        type: 'string',
-        describe: 'the path to the config file',
-        demandOption: true
-      }),
-    (argv) => startServer(argv.config)
+const cwd = process.cwd()
+const require = createRequire(resolve(cwd, 'index.js'))
+try {
+  const resolved = require.resolve('@chijs/cli')
+  if (resolved === fileURLToPath(import.meta.url)) throw 0
+  console.log(chalk.blue('Local Chi CLI detected.'))
+  fork(resolved, { execArgv: process.argv.slice(2) })
+} catch {
+  const dir = dirname(fileURLToPath(import.meta.url))
+  const { version } = JSON.parse(
+    readFileSync(join(dir, '..', './package.json'), 'utf8')
   )
-  .demandCommand()
-  .recommendCommands()
-  .strict()
-  .parse()
+
+  yargs(hideBin(process.argv))
+    .version(version)
+    .scriptName('chi')
+    .command(
+      'serve <config>',
+      'Start Chi Server',
+      (yargs) =>
+        yargs.positional('config', {
+          type: 'string',
+          describe: 'the path to the config file',
+          demandOption: true
+        }),
+      (argv) => startServer(argv.config)
+    )
+    .demandCommand()
+    .recommendCommands()
+    .strict()
+    .parse()
+}
