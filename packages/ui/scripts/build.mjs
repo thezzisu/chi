8dd1cca9963a5048ca4f36857f318879186750ce
thezzisu <thezzisu@gmail.com>
2022-06-27 16:50:32 +0800

    chore: workaround for zx with wsl

--- packages/ui/scripts/build.mjs
+++ packages/ui/scripts/build.mjs
@@ -1,5 +1,10 @@
 import { $, argv } from 'zx'
 
+if (process.platform === 'win32') {
+  $.shell = 'cmd.exe'
+  $.prefix = ''
+}
+
 await $`yarn build`
 if (argv.ci) {
   await $`yarn build -m electron`
